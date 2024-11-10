0ac4bc213fef47931cd180bf2b34c6cec5c908ea
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2024-11-10 11:00:26 +0500

    Create Service layer and Repository layer

---
+++ Entities/Models/Company.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models;
+
+public class Company
+{
+    [Column("CompanyId")]
+    public Guid Id { get; set; }
+    [Required(ErrorMessage = "Адрес компании – обязательное поле.")]
+    [MaxLength(60, ErrorMessage = "Адрес компании для адреса состоит из 60 символов.")]
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+    public string? Country { get; set; }
+    public ICollection<Employee>? Employees { get; set; }
+}
---
+++ Entities/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models;
+
+public class Employee
+{
+    [Column("EmployeeId")]
+    public Guid Id { get; set; }
+    [Required(ErrorMessage = "Имя сотрудника – обязательное поле.")]
+    [MaxLength(30,ErrorMessage = "Максимальная длина имени — 30 символов..")]
+    public string? Name { get; set; }
+    [Required(ErrorMessage = "Возраст – обязательное поле.")]
+    public int Age { get; set; }
+    [Required(ErrorMessage = "Должность – обязательное поле.")]
+    [MaxLength(20, ErrorMessage = "Максимальная длина позиции — 20 символов.")]
+    public string? Position { get; set; }
+    [ForeignKey(nameof(Company))]
+    public Guid CompanyId { get; set; }
+    public Company? Company { get; set; }
+}
